2b1e37ede11956452b0c77030b93d612ad069596
sgorban <gorban22@mail.ru>
2021-03-12 15:26:51 +0300

    1) changed number of warmup cycles 2) remove ctxt switching logging

--- src/main/java/main/LatencyMeasurer.java
+++ src/main/java/main/LatencyMeasurer.java
@@ -5,18 +5,13 @@ import net.openhft.affinity.AffinitySupport;
 import org.agrona.concurrent.ShutdownSignalBarrier;
 import picocli.CommandLine;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.Instant;
 
 public class LatencyMeasurer
     implements Runnable
 {
-    private static final Boolean CTXT_SWITCH_LOGGING_ENABLED = Boolean.getBoolean("ctxtSwitchLog");
-
     @CommandLine.Option(names = {"--warmupCycles"}, description = "Number of warmup cycles")
-    private int warmupCycles = 20_000;
+    private int warmupCycles = 200_000;
 
     @CommandLine.Option(names = {"--thresholdNs"}, description = "Threshold in nanoseconds. When latency is above " +
             "this threshold we will print this event")
@@ -56,46 +51,21 @@ public class LatencyMeasurer
                     affinityLock.cpuId());
         }
         for (int i = 0; i < warmupCycles; i++) {
-            measureLatency(Long.MAX_VALUE, threadId);
+            measureLatency(Long.MAX_VALUE);
         }
-        System.out.println("Warmup finished");
+        System.out.println("\nWarmup finished " + Instant.now());
         while (true) {
-            measureLatency(thresholdNs, threadId);
+            measureLatency(thresholdNs);
         }
     }
 
-    private static void measureLatency(final long thresholdNs, final int threadId)
+    private static void measureLatency(final long thresholdNs)
     {
         long start = System.nanoTime();
         long end = System.nanoTime();
         long latency = end - start;
         if (latency > thresholdNs) {
             System.out.println(String.format("\n\n %s Latency is greater than treshold: %d", Instant.now(), latency));
-            if (CTXT_SWITCH_LOGGING_ENABLED) {
-                System.out.println(checkCtxSwitches(threadId));
-            }
         }
-    }
-
-    private static String checkCtxSwitches(final int threadId)
-    {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("/bin/bash", "-c", String.format("cat /proc/%d/status | grep ctxt", threadId));
-        String result = "";
-        try {
-
-            final Process process = processBuilder.start();
-
-            final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result = String.join("\n", result, line);
-            }
-            process.waitFor();
-        } catch (InterruptedException | IOException e) {
-            e.printStackTrace();
-        }
-        return result;
     }
 }
