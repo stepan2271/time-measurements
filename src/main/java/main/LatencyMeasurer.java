81b9721d1b3b2e995eccc2df5c37ec287a89ba90
sgorban <gorban22@mail.ru>
2021-03-12 14:04:40 +0300

    added logging of ctxt switches

--- src/main/java/main/LatencyMeasurer.java
+++ src/main/java/main/LatencyMeasurer.java
@@ -4,14 +4,18 @@ import net.openhft.affinity.AffinityLock;
 import net.openhft.affinity.AffinitySupport;
 import picocli.CommandLine;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class LatencyMeasurer
 {
     @CommandLine.Option(names = {"--warmupCycles"}, description = "Number of warmup cycles")
-    private int warmupCycles = 20_000;
+    private int warmupCycles = 10_000;
 
     @CommandLine.Option(names = {"--thresholdNs"}, description = "Threshold in nanoseconds. When latency is above " +
             "this threshold we will print this event")
-    private long thresholdNs = 500_000_000L;
+    private long thresholdNs = 100_000;
 
     @CommandLine.Option(names = {"--bindingCpu"}, description = "Binding cpu")
     private int bindingCpu = -1;
@@ -27,37 +31,64 @@ public class LatencyMeasurer
     private void run()
     {
         final Thread thread = Thread.currentThread();
+        final int threadId = AffinitySupport.getThreadId();
+        System.out.printf("Thread id: %d\n", threadId);
         if (bindingCpu != -1) {
             System.out.printf(
                     "\nAttempt to bind thread %d (%d) to cpu %d",
                     thread.getId(),
-                    AffinitySupport.getThreadId(),
+                    threadId,
                     bindingCpu);
             final AffinityLock affinityLock = AffinityLock.acquireLock(bindingCpu);
             System.out.printf(
                     "\nSuccessfully bound thread %d (%d) to cpu %d",
                     thread.getId(),
-                    AffinitySupport.getThreadId(),
+                    threadId,
                     affinityLock.cpuId());
         }
-        for (int i = 0; i < warmupCycles; i++)
-        {
-            measureLatency(Long.MAX_VALUE);
+        System.out.println(checkCtxSwitches(threadId));
+        for (int i = 0; i < warmupCycles; i++) {
+            measureLatency(Long.MAX_VALUE, threadId);
         }
-        while (true)
-        {
-            measureLatency(thresholdNs);
+        System.out.println("Warmup finished");
+        System.out.println(checkCtxSwitches(threadId));
+        while (true) {
+            measureLatency(thresholdNs, threadId);
         }
     }
 
-    private static void measureLatency(final long thresholdNs)
+    private static void measureLatency(final long thresholdNs, final int threadId)
     {
         long start = System.nanoTime();
         long end = System.nanoTime();
         long latency = end - start;
-        if (latency > thresholdNs)
-        {
-            System.out.println(String.format("Latency is greater than treshold: %d", latency));
+        if (latency > thresholdNs) {
+            final String ctxtSwitches = checkCtxSwitches(threadId);
+            System.out.println(String.format("\n\n Latency is greater than treshold: %d", latency));
+            System.out.println(ctxtSwitches);
         }
+    }
+
+    private static String checkCtxSwitches(final int threadId)
+    {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("/bin/bash", "-c", String.format("cat /proc/%d/status | grep ctxt", threadId));
+        String result = "";
+        try {
+
+            final Process process = processBuilder.start();
+
+            final BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result = String.join("\n", result, line);
+            }
+            process.waitFor();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
